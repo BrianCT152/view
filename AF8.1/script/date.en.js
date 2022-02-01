6a9e56a4bbd85dfd3bc925fb066d0a80882aac38
BrianCT152 <98082100+BrianCT152@users.noreply.github.com>
2022-02-01 13:54:03 +0100

    Add files via upload

---
+++ AF8.1/script/date.en.js
@@ -0,0 +1,14 @@
+﻿var months = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+
+var WeekDays = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+
+
+
+var f = new Date();  //javascript
+
+var strDate = WeekDays[f.getDay()] + " - " + months[f.getMonth()] + " " + f.getDate() + ", " + f.getFullYear();
+
+$(function () {
+    $('footer span').last().text(f.getFullYear());
+});
+
